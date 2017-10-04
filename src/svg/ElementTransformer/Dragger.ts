30d0183e891d2776b673b794d334e0ee115aa177
soloproyectos <gonzalo.chumillas@gmail.com>
2017-10-04 20:10:10 +0200

    change opacity

--- src/svg/ElementTransformer/Dragger.ts
+++ src/svg/ElementTransformer/Dragger.ts
@@ -8,7 +8,7 @@ export class Dragger extends SvgGraphicElement {
 
     this
       .setAttr("fill", "000")
-      .setAttr("opacity", .5);
+      .setAttr("opacity", 0);
   }
 
   get position(): Point {
