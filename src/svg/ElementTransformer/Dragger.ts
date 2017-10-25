9f1b0d84c693a22947fa50d937b30be471d705a7
soloproyectos <gonzalo.chumillas@gmail.com>
2017-10-25 21:38:51 +0200

    recode

--- src/svg/ElementTransformer/Dragger.ts
+++ src/svg/ElementTransformer/Dragger.ts
@@ -3,12 +3,15 @@ import {Vector} from "../../euclidean/dim2/Vector";
 import {SvgGraphicElement} from "./../SvgGraphicElement";
 
 export class Dragger extends SvgGraphicElement {
+  private _backgroundColor = "000";
+  private _opacity = 0;
+
   constructor() {
     super("rect");
 
     this
-      .setAttr("fill", "000")
-      .setAttr("opacity", 0.5);
+      .setAttr("fill", this._backgroundColor)
+      .setAttr("opacity", this._opacity);
   }
 
   get width(): number {
