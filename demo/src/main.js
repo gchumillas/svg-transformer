770daee7e3164ed850716680d7092c33e6bc7c8b
soloproyectos <gonzalo.chumillas@gmail.com>
2017-11-06 18:02:17 +0100

    update demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -6,8 +6,23 @@ require.config({
 });
 
 define(['SvgTransformer'], function (SvgTransformer) {
-  var elements = document.querySelectorAll('#my-canvas > *');
-  var t = new SvgTransformer();
+  var elements = document.querySelectorAll('#my-canvas > #path1, #rect1');
+  var items = [];
+  for (var element of elements) {
+    items.push(element);
+  }
 
-  t.show(elements);
+  var t = new SvgTransformer();
+  t.neckLength = 50;
+  t.stroke = "red";
+  t.strokeWidth = 3;
+  t.handleRadius = 15;
+  t.isDraggable = true;
+  t.isResizable = true;
+  t.isAspectRatioPreserved = false;
+  t.isRotable = true;
+  t.show(items);
+
+  var t1 = new SvgTransformer();
+  t1.show(document.querySelector("#rect2"));
 });
